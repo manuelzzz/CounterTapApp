e5542392246d2283100807846609b3a918971b1b
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-02-22 18:31:28 -0300

    Challenge time!

--- CounterTapApp/ViewController.swift
+++ CounterTapApp/ViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
+// challenge conditions:
+// while number >= 10 - background color should be red
+// while number >= 20 - background color should be blue
+// when number == 30 - reset the counter and put the background color white again
+
 class ViewController: UIViewController {
 
 	@IBOutlet weak var numberLabel: UILabel!
 	@IBOutlet weak var changeCounterButton: UIButton!
+
 	var currentNumber: Int = 0
 
 	override func viewDidLoad() {
@@ -30,8 +36,25 @@ class ViewController: UIViewController {
 		)
 	}
 
+	func changeViewBackgroundColor(to color: UIColor) {
+		view.backgroundColor = color
+	}
+
 	@IBAction func changeCounterButtonTapped(_ sender: Any) {
 		currentNumber += 1
+
+		switch currentNumber {
+		case 0..<11:
+			changeViewBackgroundColor(to: .white)
+		case 10..<21:
+			changeViewBackgroundColor(to: .blue)
+		case 20..<31:
+			changeViewBackgroundColor(to: .red)
+		default:
+			currentNumber = 0
+			changeViewBackgroundColor(to: .white)
+		}
+
 		numberLabel.text = "\(currentNumber)"
 	}
 
