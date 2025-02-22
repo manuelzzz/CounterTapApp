881eb84cf9670865631c11838cec0d5e78e0fa26
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-02-22 18:13:22 -0300

    Adding the logic to update number

--- CounterTapApp/ViewController.swift
+++ CounterTapApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var numberLabel: UILabel!
+	@IBOutlet weak var changeCounterButton: UIButton!
+	var currentNumber: Int = 0
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		numberLabel.frame = CGRect(
+			x: 0,
+			y: numberLabel.frame.origin.y,
+			width: view.frame.width,
+			height: 100
+		)
+
+		let changeCounterButtonYPosition = numberLabel.frame.origin.y + numberLabel.frame.height + 40
+
+		changeCounterButton.frame.origin = CGPoint(
+			x: changeCounterButton.frame.origin.x,
+			y: changeCounterButtonYPosition
+		)
 	}
 
+	@IBAction func changeCounterButtonTapped(_ sender: Any) {
+		currentNumber += 1
+		numberLabel.text = "\(currentNumber)"
+	}
 
 }
-
